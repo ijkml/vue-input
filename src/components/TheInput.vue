a207bab8a5507b6c1479d2ba4949bee02edcdd29
ML <me.mlaure@gmail.com>
2022-12-31 23:23:20 +0100

    update deps

--- src/components/TheInput.vue
+++ src/components/TheInput.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
+type ValidateOptions = 'eager' | 'lazy' | 'none';
+type InputType = 'text' | 'password' | 'email' | 'number';
 // interface Props {
 //   label?: string
 // }
@@ -15,13 +17,18 @@ const inputLabel = 'Form Input Label';
 const inputPlaceholder = 'Form Input Placeholder';
 const inputBg = '';
 const inputColor = '';
-const inputActiveColor = 'hotpink';
+const inputActiveColor = '';
+const inputErrorColor = '';
+
+const validate = true; // eager | lazy | none
+// type = email, password, email
 
 // ------ possible props -----------//
 
 const elem = ref<HTMLInputElement | null>(null);
 
 const isFocused = ref(false);
+const hasError = ref(false);
 
 const inputValue = ref('');
 
@@ -35,46 +42,94 @@ function setFocus(on: boolean) {
 
   isFocused.value = false;
 }
+
+function onBlur() {
+  setFocus(false);
+  if (validate) {
+    hasError.value = true;
+  }
+}
+
+function onFocus() {
+  setFocus(true);
+  hasError.value = false;
+}
 </script>
 
 <template>
-  <div class="input-box" :class="{ focus: isFocused }">
-    <label class="input-label">{{ inputLabel }}</label>
-    <input
-      v-model.trim="inputValue"
-      type="text"
-      class="input-1"
-      :placeholder="inputPlaceholder"
-      @focus="setFocus(true)"
-      @blur="setFocus(false)"
-    >
+  <div class="input-container">
+    <div class="input-box" :class="{ focus: isFocused, error: hasError }">
+      <label class="input-label">{{ inputLabel }}</label>
+      <input
+        v-model.trim="inputValue"
+        type=""
+        class="input-1"
+        :placeholder="inputPlaceholder"
+        @focus="onFocus"
+        @blur="onBlur"
+      >
+    </div>
+    <div class="message-box">
+      Error message
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.input-box {
-  @apply relative z-auto mx-10px;
+.input-container {
+  @apply mx-10px;
 
   // default bg-color
   --default-bg-color: hsl(0, 0%, 100%);
   // default text color
   --default-tx-color: hsl(210, 29%, 24%);
+  // default error color
+  --default-err-color: hsl(349, 100%, 35%);
 
   .dark & {
     // default dark-mode bg-color
     --default-bg-color: hsl(0, 0%, 7%);
     // default dark-mode text color
     --default-tx-color: hsla(0, 0%, 92%, 0.7);
+    // default dark-mode error color
+    --default-err-color: hsl(4, 90%, 58%);
   }
 
   --prop-bg-color: v-bind(inputBg); // bg color
   --prop-tx-color: v-bind(inputColor); // text color
   --prop-active-color: v-bind(inputActiveColor); // active color
+  --prop-error-color: v-bind(inputErrorColor); // error color
 
   // --- exposed with defaults
   --input-bg-color: var(--prop-bg-color, var(--default-bg-color));
   --input-text-color: var(--prop-tx-color, var(--default-tx-color));
   --input-active-color: var(--prop-active-color, currentColor);
+  --input-error-color: var(--prop-error-color, var(--default-err-color));
+}
+
+.input-box {
+  @apply relative z-auto;
+
+  &.focus, &.error {
+    .input-label {
+      top: -8px;
+      background: var(--input-bg-color);
+      font-size: 11px;
+      transform: translateY(0%);
+    }
+  }
+
+  &.focus {
+    .input-label {
+      color: var(--input-active-color);
+    }
+  }
+
+  &.error {
+    .input-label {
+      color: var(--input-error-color);
+    }
+  }
 }
 
 .input-label {
@@ -99,8 +154,13 @@ function setFocus(on: boolean) {
   color: var(--input-text-color);
 
   &:focus {
-    outline: solid 2px;
+    outline: solid 1.5px;
     outline-color: var(--input-active-color);
+  }
+
+  .input-box.error & {
+    outline: solid 1px;
+    outline-color: var(--input-error-color);
   }
 
   &::placeholder,
@@ -116,31 +176,7 @@ function setFocus(on: boolean) {
   }
 }
 
-.input-box.focus {
-  .input-label {
-    color: var(--input-active-color);
-    top: -8px;
-    background: var(--input-bg-color);
-    font-size: 11px;
-    transform: translateY(0%);
-
-    transition: 250ms;
-  }
-}
-
-// forget below
-
-.input-box.error {
-  .input-label {
-    // color: $error-color; // #f44336
-    top: -8px;
-    // background: $default-background;
-    font-size: 11px;
-    transition: 250ms;
-  }
-
-  .input-1 {
-    // border: 2px solid $error-color; // #f44336
-  }
+.message-box {
+  @apply text-xs my-1 px-4 pt-2;
 }
 </style>
